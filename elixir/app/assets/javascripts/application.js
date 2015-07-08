6a45947af2cc6952753cfbb707385c83c6fb17e2
Elisabeth McKoy <elisabeth.mckoy@gmail.com>
2015-07-08 14:59:09 +1200

    click function working

--- elixir/app/assets/javascripts/application.js
+++ elixir/app/assets/javascripts/application.js
@@ -12,16 +12,20 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
 
 $(document).ready(function(){
   function colorGradient(){
     colorString = "";
     for (var i = 0; i < 500; ++i){
+      //if no color is being added do this
       r = Math.sin(.3 * i + 0) * (50) + 200;
       g = Math.sin(.4 * i + 2) * (50) + 200;
       b = Math.sin(.3 * i + 4) * (50) + 200;
+      //if color is being added set r, g, b to users color
+      // r = first
+      // g = second
+      // b = third
       div ='<div class="color" style="background-color:rgb({r},{g},{b});"></div>';
       div = div.replace("{r}",Math.floor(r));
       div = div.replace("{g}",Math.floor(g));
@@ -30,12 +34,8 @@ $(document).ready(function(){
     }
     return colorString ;
   }
+
   $('#rainbowish').html(colorGradient());
-  
-  setInterval(function () {
-    rotateDivsBack();
-    // rotateDivsForward();
-  }, 50 );
 
   var rotateDivsBack = function() {
     var $removedDiv = $('#rainbowish').children().first().remove();
@@ -46,6 +46,35 @@ $(document).ready(function(){
     var $removedDiv = $('#rainbowish').children().last().remove();
     $("#rainbowish").prepend($removedDiv);
   }
+
+    var back;
+    var forward;
+  $(document).on('click', function(){
+    var clicks = $(this).data('clicks');
+
+    if (clicks){
+      clearInterval(forward);
+      back = setInterval(function () {
+        rotateDivsBack();
+      }, 50 );
+
+    } else {
+      clearInterval(back);
+      forward = setInterval(function () {
+        rotateDivsForward();
+      }, 50 );
+    }
+    $(this).data("clicks", !clicks);
+  })
+
+
+  // generateOpacity = function (opacity) {
+  //   var $div = $("<div class='color'></div>").css({
+  //     // use users colors for rgb values?
+  //     'background' : 'rgba(255,0,0,' + opacity / 255 + ')'
+  //   });
+  //   return $div;
+  // };
 
   // pass ruby objects to javascript
   // var first = '<%= @pattern.first %>';
